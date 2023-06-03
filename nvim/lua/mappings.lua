768863d4db245b1991d19e41da9ae8ad787f51b5
aakloul <aakloul@users.noreply.github.com>
2023-06-03 19:05:49 +0800

    whichkey mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,21 +6,21 @@ vim.g.mapleader = " "
 
 -- nvim-tree keymappings
 -- map the key n to run the command :NvimTreeToggle
-map('n', 'n', [[:NvimTreeToggle<CR>]], {})
+-- map('n', 'n', [[:NvimTreeToggle<CR>]], {})
 
 -- nvim-dapui keymappings
 -- Press Ctrl+d to toggle debug mode, will remove NvimTree also
-map('n', '<C-d>', [[:NvimTreeClose<CR> :lua require'dapui'.toggle()<CR>]], {})
+map("n", "<C-d>", [[:NvimTreeClose<CR> :lua require'dapui'.toggle()<CR>]], {})
 --map('n', '<C-d>', [[:NvimTreeToggle<CR> :lua require'dapui'.toggle()<CR>]], {})
 -- Press F6 to open REPL
-map('n', '<F6>', [[:lua require'dap'.repl.open()<CR>]], {})
+map("n", "<F6>", [[:lua require'dap'.repl.open()<CR>]], {})
 -- Press dl to run last ran configuration (if you used f5 before it will re run it etc)
-map('n', 'dl', [[:lua require'dap'.run_last()<CR>]], {})
+map("n", "dl", [[:lua require'dap'.run_last()<CR>]], {})
 
 -- define common options
 local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
+	noremap = true, -- non-recursive
+	silent = true, -- do not show message
 }
 
 -----------------
@@ -29,10 +29,10 @@ local opts = {
 
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 -- delta: 2 lines
@@ -46,6 +46,5 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 -----------------
 
 -- Hint: start visual mode with the same area as the previous area and the same mode
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
-
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
